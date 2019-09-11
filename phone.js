8abf037dd7a809490f164a5d0373fe1890afe21b
xiaoxiaoqing <747904295@qq.com>
2019-09-11 18:35:31 +0800

    成

--- phone.js
+++ phone.js
@@ -2,4 +2,13 @@ $(function () {
 // 根据屏幕的大小设置字体的大小
 var fs = document.documentElement.offsetWidth;
 $('html').css({'fontSize':fs*10/828});
+	$('.ying').on('click',function(){
+		$('.lay').show();
+	});
+	$('.close').on('click',function(){
+		$('.lay').hide();
+	})
+	$('.pdf').on('click',function(){
+		$('.main').html('<img class="tu" src="lxq.jpg" alt="">')
+	})
 })
